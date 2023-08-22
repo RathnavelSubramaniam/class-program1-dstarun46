7c7421179ceb1d67f2eee3d36df7762e1e1bb67c
dstarun46 <142779824+dstarun46@users.noreply.github.com>
2023-08-22 09:34:24 +0000

    completed

---
+++ oddoreven.java
@@ -0,0 +1,9 @@
+class oddoreven{
+public static void main(String[]args){
+int num=10;
+if(num%2!=0)
+System.out.println("This given number is ODD");
+else
+System.out.println("This given number is EVEN");
+}
+}
